fccfbbfc5f31eba04505ebaf426816eed9b39186
Werner Pieterson <wernerhp@gmail.com>
2022-07-10 22:28:28 +0200

    Migrate config entry and fix small bug

--- custom_components/load_shedding/__init__.py
+++ custom_components/load_shedding/__init__.py
@@ -243,3 +243,31 @@ class LoadSheddingScheduleUpdateCoordinator(DataUpdateCoordinator[dict[str, Any]
                 )
 
             return {**{ATTR_STAGE: stage, ATTR_SCHEDULE: data}}
+
+
+async def async_migrate_entry(hass, config_entry: ConfigEntry):
+    """Migrate old entry."""
+    _LOGGER.debug("Migrating from version %s", config_entry.version)
+
+    if config_entry.version == 1:
+        old = {**config_entry.data}
+        suburbs = old.get("suburbs")
+        new = {
+            CONF_STAGE: {
+                CONF_PROVIDER: Provider.ESKOM.value,
+            },
+            CONF_AREAS: [
+                {
+                    CONF_DESCRIPTION: suburbs[0].get(CONF_DESCRIPTION),
+                    CONF_AREA: suburbs[0].get("suburb"),
+                    CONF_AREA_ID: suburbs[0].get("suburb_id"),
+                    CONF_PROVIDER: Provider.ESKOM.value,
+                    CONF_PROVINCE_ID: suburbs[0].get(CONF_PROVINCE_ID),
+                }
+            ],
+        }
+        config_entry.version = 2
+        hass.config_entries.async_update_entry(config_entry, data=new)
+
+    _LOGGER.info("Migration to version %s successful", config_entry.version)
+    return True
